5d2fd06b58700deb951ad179a58c78f08a1fdaf3
Ousmane-Mo <ousmanebalde33150@gmail.com>
2024-10-21 18:00:52 +0200

    project init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>
+          
+        </header>
         {children}
       </body>
     </html>
